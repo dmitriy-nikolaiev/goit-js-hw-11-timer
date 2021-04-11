ab83d5f38da5d276158233c65b9900d3018be828
Dmitriy Nikolaiev <dimnik.u11@gmail.com>
2021-04-11 21:28:25 +0300

    Добавил исправления

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import './styles.css';
 
 class CountdownTimer {
-  #element;
-  #timerId = null;
+  // #timerId = null;
 
   constructor(selector, targetDate) {
     this.selector = selector;
     this.targetDate = targetDate;
   }
+
+  timerId = setInterval(() => {
+    this.render();
+  }, 1000);
 
   transformValues(value) {
     return String(value).padStart(2, '0');
@@ -17,7 +20,7 @@ class CountdownTimer {
     const time = this.targetDate - new Date();
 
     if (time <= 0) {
-      clearInterval(this.#timerId);
+      clearInterval(this.timerId);
       return;
     }
     const days = Math.floor(time / (1000 * 60 * 60 * 24));
@@ -25,28 +28,28 @@ class CountdownTimer {
     const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
     const secs = Math.floor((time % (1000 * 60)) / 1000);
 
-    this.#element.querySelector(
+    const element = document.querySelector(this.selector);
+    element.querySelector(
       '[data-value="secs"]',
     ).textContent = this.transformValues(secs);
-    this.#element.querySelector(
+    element.querySelector(
       '[data-value="mins"]',
     ).textContent = this.transformValues(mins);
-    this.#element.querySelector(
+    element.querySelector(
       '[data-value="hours"]',
     ).textContent = this.transformValues(hours);
-    this.#element.querySelector(
+    element.querySelector(
       '[data-value="days"]',
     ).textContent = this.transformValues(days);
   }
 
-  init() {
-    this.#element = document.querySelector(this.selector);
-
-    this.#timerId = setInterval(() => {
-      this.render();
-    }, 1000);
-  }
+  // init() {
+  //   this.#timerId = setInterval(() => {
+  //     this.render();
+  //   }, 1000);
+  // }
 }
 
-const timer = new CountdownTimer('#timer-1', new Date('Jul 17, 2021'));
-timer.init();
+// const timer = new CountdownTimer('#timer-1', new Date('Jul 17, 2021'));
+// timer.init();
+new CountdownTimer('#timer-1', new Date('Jul 17, 2021'));
